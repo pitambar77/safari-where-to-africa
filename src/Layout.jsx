cd353dd529f48a2541ad162fd31fa41b4ef0bc5e
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2025-10-23 21:09:41 +0530

    layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from './components/header'
 import { Outlet } from 'react-router-dom'
 import Testimonial from './components/Testimonial'
 import TalkExpert from './components/TalkExpert'
 import Footer from './components/Footer'
 import Partner from './components/Partner'
+import Header from './components/Header'
 
 const Layout = () => {
   return (
